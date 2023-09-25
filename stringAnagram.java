09a6444ec7ee6f727e23f58f50ade4836c29977b
Hacker Ram <106467573+hacker-ram@users.noreply.github.com>
2023-09-26 02:38:17 +0530

    string

---
+++ stringAnagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class stringAnagram {
+    public static void main(String[] args) {
+        String str1 = "Master school";
+        String str2 = "The classroom ";
+
+        str1= str1.replace(" ", "");
+        str2= str2.replace(" ", "");
+
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        char ar1[]=str1.toCharArray();
+        char ar2[]=str2.toCharArray();
+
+        Arrays.sort(ar1);
+        Arrays.sort(ar2);
+
+        if (Arrays.equals(ar1, ar2)) {
+            System.out.println("It is an Anagram");
+        } else {
+            System.out.println("It is not an Anagram");
+            
+        }
+    }
+}
