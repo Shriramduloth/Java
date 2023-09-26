5146356042ea2956001a8e3aa9c9410a9bdaf707
Hacker Ram <106467573+hacker-ram@users.noreply.github.com>
2023-09-26 11:47:44 +0530

    string

---
+++ mutableString2.java
@@ -0,0 +1,19 @@
+public class mutableString2 {
+    public static void main(String[] args) {
+        // final int a= 10;
+        // a=20;
+        // System.out.println(a);
+
+        // StringBuffer sb= new StringBuffer("Ram ");
+        // sb.append("Yadav");
+        // System.out.println(sb);
+        // sb=new StringBuffer("sahil");
+        // System.out.println(sb);
+        final StringBuffer sb = new StringBuffer("Ram ");
+        sb.append("Yadav");
+        System.out.println(sb);
+        sb = new StringBuffer("sahil");
+        System.out.println(sb);
+    }
+
+}
