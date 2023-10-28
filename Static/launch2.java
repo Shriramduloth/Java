b974f10976a8a825241cad50aed2563cfd07e996
Hacker Ram <106467573+hacker-ram@users.noreply.github.com>
2023-10-28 17:51:21 +0530

    static keyword

---
+++ Static/launch2.java
@@ -0,0 +1,22 @@
+package Static;
+
+
+class Demo{
+    static void disp1(){
+        System.out.println("Static display 1");
+    }
+    void disp2 (){
+        System.out.println("Non static display 2");
+    }
+    
+}
+
+public class launch2 {
+public static void main(String[] args) {
+        Demo.disp1();
+        //Demo.disp2();
+        Demo d = new Demo();
+        d.disp1();
+        d.disp2();
+    }
+}
